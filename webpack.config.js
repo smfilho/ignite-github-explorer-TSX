b3a7b0ee4550da4d0595b722e997edd5bd944421
Silvio Melo Filho <smfilho@gmail.com>
2021-09-14 23:30:31 -0500

    added: webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    hot: true,
+  },
+  // devServer: {
+  //   static: path.resolve(__dirname, 'public'),
+
+  // },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
