2036020682fde436f9630c58171f0daa873a3958
Silvio Melo Filho <smfilho@gmail.com>
2021-09-15 12:03:05 -0500

    added: source amps, SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -19,10 +22,6 @@ module.exports = {
     },
     hot: true,
   },
-  // devServer: {
-  //   static: path.resolve(__dirname, 'public'),
-
-  // },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
@@ -32,6 +31,11 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node-modules/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node-modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
